e6f3ff106d4ef3570b61748993f712af3a5227b2
schneefux <schneefux+commit@schneefux.xyz>
2017-03-06 21:15:26 +0100

    fix: nevermind. DESC -> ASC

--- joblib.py
+++ joblib.py
@@ -76,7 +76,7 @@ class JobQueue(object):
                             FROM (
                                 SELECT id FROM jobs
                                 WHERE status='open' AND type=$1
-                                ORDER BY priority DESC
+                                ORDER BY priority ASC
                                 LIMIT $2
                             ) AS open_jobs
                             WHERE jobs.id=open_jobs.id
